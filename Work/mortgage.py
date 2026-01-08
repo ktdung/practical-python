df259b3caac51609c751b90d787c04caf4762657
ktdung <20261869+ktdung@users.noreply.github.com>
2026-01-08 21:53:32 +0700

    01: introduction Exercise 1.10: Making a table

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -9,8 +9,9 @@ payment = 2684.11
 total_paid = 0.0
 
 month = 0
-extra_payment = 1000.0
-extra_payment_months = 12
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
     month = month + 1
@@ -20,18 +21,18 @@ while principal > 0:
     principal = principal + interest
 
     # Bước 2: Xác định số tiền trả
-    if month <= extra_payment_months:
+    current_pay = payment
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
         current_pay = payment + extra_payment
-    else:
-        current_pay = payment
 
-    # Bước 3: Xử lý tháng cuối
-    if principal < current_pay:
-        current_pay = principal
+    # # Bước 3: Xử lý tháng cuối
+    # if principal < current_pay:
+    #     current_pay = principal
 
     # Bước 4: Trừ nợ và cộng dồn tiền đã trả
     principal = principal - current_pay
     total_paid = total_paid + current_pay
+    print(month, round(total_paid, 2), round(principal, 2))
 
 # In kết quả với 2 chữ số thập phân
 print('Total paid:', round(total_paid, 2))
