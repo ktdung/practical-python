5a2d89d8933fff9d676eee253fa5399326da5fd4
ktdung <20261869+ktdung@users.noreply.github.com>
2026-01-12 15:39:33 +0700

    01: introduction Exercise 1.27: Reading a data file

--- Work/demo.py
+++ Work/demo.py
@@ -1,5 +1,2 @@
-a = 37
-b = -299392993727716627377128481812241231
-c = 0x7fa8      # Hexadecimal
-d = 0o253       # Octal
-e = 0b10001111  # Binary
+with open('outfile', 'wt') as f:
+  f.write('Hello, World!\n')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+with open("Data/portfolio.csv", 'rt') as file:
+  total_cost = 0.0
+  next(file)
+  for line in file:
+    parts = line.split(',')
+    shares = int(parts[1])
+    price = float(parts[2])
+    total_cost += shares * price
+  print(f'Total cost: {total_cost}')
