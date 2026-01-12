d51aac91efab3dc74aca849879a471810d248bf6
ktdung <20261869+ktdung@users.noreply.github.com>
2026-01-12 21:47:35 +0700

    01: introduction Exercise Exercise 1.32: Using a library function

--- Work/pcost.py
+++ Work/pcost.py
@@ -20,12 +20,32 @@
 
 
 # Exercise 1.30
+# def portfolio_cost(filename):
+#   total_cost = 0.0
+#   with open(filename, 'rt') as file:
+#     next(file)
+#     for line in file:
+#       parts = line.split(',')
+#       try:
+#         shares = int(parts[1])
+#         price = float(parts[2])
+#       except ValueError:
+#         print('Error: There was a problem with the data format in the file.')
+#         continue
+#       total_cost += shares * price
+#   return total_cost
+
+# cost = portfolio_cost('Data/missing.csv')
+# print('Total cost:', cost)
+
+# Exercise 1.32: Using a library function
+import csv
 def portfolio_cost(filename):
   total_cost = 0.0
   with open(filename, 'rt') as file:
-    next(file)
-    for line in file:
-      parts = line.split(',')
+    reader = csv.reader(file)
+    next(reader)
+    for parts in reader:
       try:
         shares = int(parts[1])
         price = float(parts[2])
@@ -35,5 +55,5 @@ def portfolio_cost(filename):
       total_cost += shares * price
   return total_cost
 
-cost = portfolio_cost('Data/missing.csv')
+cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost:', cost)
