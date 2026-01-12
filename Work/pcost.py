a61202d7f9c99031db8858792ab3ae38ff18c08d
ktdung <20261869+ktdung@users.noreply.github.com>
2026-01-12 15:43:06 +0700

    01: introduction Exercise 1.28: Reading gzip

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
-with open("Data/portfolio.csv", 'rt') as file:
-  total_cost = 0.0
-  next(file)
+# with open("Data/portfolio.csv", 'rt') as file:
+#   total_cost = 0.0
+#   next(file)
+#   for line in file:
+#     parts = line.split(',')
+#     shares = int(parts[1])
+#     price = float(parts[2])
+#     total_cost += shares * price
+#   print(f'Total cost: {total_cost}')
+
+# Exercise 1.28
+import gzip
+
+with gzip.open("Data/portfolio.csv.gz", "rt") as file:
   for line in file:
-    parts = line.split(',')
-    shares = int(parts[1])
-    price = float(parts[2])
-    total_cost += shares * price
-  print(f'Total cost: {total_cost}')
+    print(line, end='')
