5068bd2d79b1a68f0f709c6b1ab913297f59bb6a
ktdung <20261869+ktdung@users.noreply.github.com>
2026-05-12 16:47:06 +0700

    Exercise 3.7: Picking a different column delimiter Although CSV files are pretty common, it’s also possible that you could encounter a file that uses a different column separator such as a tab or space. For example, the file Data/portfolio.dat looks like this:
    name shares price
    "AA" 100 32.20
    "IBM" 50 91.10
    "CAT" 150 83.44
    "MSFT" 200 51.23
    "GE" 95 40.37
    "MSFT" 50 65.10
    "IBM" 100 70.44
    The csv.reader() function allows a different column delimiter to be given as follows:
    
    rows = csv.reader(f, delimiter=' ')
    Modify your parse_csv() function so that it also allows the delimiter to be changed.
    
    For example:
    
    >>> portfolio = parse_csv('Data/portfolio.dat', types=[str, int, float], delimiter=' ')
    >>> portfolio
    [{'name': 'AA', 'shares': 100, 'price': 32.2}, {'name': 'IBM', 'shares': 50, 'price': 91.1}, {'name': 'CAT', 'shares': 150, 'price': 83.44}, {'name': 'MSFT', 'shares': 200, 'price': 51.23}, {'name': 'GE', 'shares': 95, 'price': 40.37}, {'name': 'MSFT', 'shares': 50, 'price': 65.1}, {'name': 'IBM', 'shares': 100, 'price': 70.44}]
    >>>

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,24 +4,27 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=","):
     """
     Parse a CSV file into a list of records
     """
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
-        # Read the file headers
-        headers = next(rows)
+        # Read the file headers has_headers is True, and use them as the keys for the dictionaries
+        if has_headers:
+            headers = next(rows)
+        else:
+            headers = []
 
         # If a column selector was given, find indices of the specified columns
         # Also narrow the set of headers useds for resuulting dictionaries
 
         if select:
-            indicies = [headers.index(colname) for colname in select]
+            indices = [headers.index(colname) for colname in select]
             headers = select
         else:
-            indicies = []
+            indices = []
 
         records = []
         for row in rows:
@@ -29,15 +32,19 @@ def parse_csv(filename, select=None, types=None):
                 continue
 
             # Filter the row if specific columns were selected
-            if indicies:
-                row = [row[index] for index in indicies]
+            if select:
+                row = [row[index] for index in indices]
 
             # Type conversion
             if types:
                 row = [func(val) for func, val in zip(types, row)]
 
-            # Make a dictionary for the row and add it to the records list
-            record = dict(zip(headers, row))
+            # If no headers, create a tuple instead of a dictionary
+            if not has_headers:
+                record = tuple(row)
+            else:
+                # Make a dictionary for the row and add it to the records list
+                record = dict(zip(headers, row))
             records.append(record)
 
         return records
