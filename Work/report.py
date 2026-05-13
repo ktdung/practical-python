719d2206be0456e18b9de9725a3320099e5cbdea
ktdung <20261869+ktdung@users.noreply.github.com>
2026-05-13 14:30:33 +0700

    Exercise 3.12: Using your library module In section 2, you wrote a program report.py that produced a stock report like this:
          Name     Shares      Price     Change
    ---------- ---------- ---------- ----------
            AA        100       9.22     -22.98
           IBM         50     106.28      15.18
           CAT        150      35.46     -47.98
          MSFT        200      20.89     -30.34
            GE         95      13.48     -26.89
          MSFT         50      20.89     -44.21
           IBM        100     106.28      35.84
    Take that program and modify it so that all of the input file processing is done using functions in your fileparse module. To do that, import fileparse as a module and change the read_portfolio() and read_prices() functions to use the parse_csv() function.
    
    Use the interactive example at the start of this exercise as a guide. Afterwards, you should get exactly the same output as before.

--- Work/report.py
+++ Work/report.py
@@ -5,60 +5,34 @@
 # Exercise 2.7: Finding out if you can retire
 # Exercise 2.9: 2:11 Collecting Data
 
-import csv
+
+
+from fileinput import filename
+
+import fileparse
 
 def read_portfolio(filename):
-  '''Reads a portfolio file and returns a list of dictionaries
-    with keys (name, shares, price) '''
-  portfolio = []
-
-  with open(filename, 'rt') as file:
-    rows = csv.reader(file)
-    headers = next(rows)
-
-    for row in rows:
-      try:
-        record = dict(zip(headers, row))
-        stock = {
-          'name': record['name'],
-          'shares': int(record['shares']),
-          'price': float(record['price'])
-        }
-        portfolio.append(stock)
-      except ValueError:
-        print('Error: There was a problem with the data format in the file.')
-        continue
-  return portfolio
+    '''
+    Reads a portfolio file and returns a list of dictionaries
+    with keys (name, shares, price)
+    '''
+    return fileparse.parse_csv(filename, select=['name', 'shares', 'price'], types=[str, int, float])
 
 def read_prices(filename):
   '''
   Read a CSV file of price data into a dict mapping names to prices.
   '''
-  prices = {}
-
-  with open(filename, 'rt') as file:
-    rows = csv.reader(file)
-    for row in rows:
-      if len(row) == 0:
-        continue
-      try:
-        name = row[0].strip().strip('"')
-        price = float(row[1])
-        prices[name] = price
-      except ValueError:
-        print('Error: There was a problem with the data format in the file.')
-        continue
-  return prices
+  return dict(fileparse.parse_csv(filename,  types=[str, float], has_headers=False))
 
 
-def make_report(portfolio, prices):
+def make_report_data(portfolio, prices):
   '''
   Make a list of (name, shares, price, change) tuples given a portfolio list and a price dict.
   '''
   rows = []
 
   for stock in portfolio:
-    current_price = prices.get(stock['name'], 0)
+    current_price = prices[stock['name']]
     change = current_price - stock['price']
     rows.append((stock['name'], stock['shares'], current_price, change))
 
@@ -84,7 +58,7 @@ def portfolio_report(portfolio_file, prices_file):
   prices = read_prices(prices_file)
 
   # Create the report data
-  report = make_report(portfolio, prices)
+  report = make_report_data(portfolio, prices)
 
   # Print it out
   print_report(report)
