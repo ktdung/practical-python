bdfd502358c767126fedf3237700b36d377c9e7a
ktdung <20261869+ktdung@users.noreply.github.com>
2026-05-14 18:03:10 +0700

    Exercise 5.6: Simple Properties Properties are a useful way to add "computed attributes" to an object. In stock.py, you created an object Stock. Notice that on your object there is a slight inconsistency in how different kinds of data are extracted:
    >>> from stock import Stock
    >>> s = Stock('GOOG', 100, 490.1)
    >>> s.shares
    100
    >>> s.price
    490.1
    >>> s.cost()
    49010.0
    >>>
    Specifically, notice how you have to add the extra () to cost because it is a method.
    
    You can get rid of the extra () on cost() if you turn it into a property. Take your Stock class and modify it so that the cost calculation works like this:
    
    >>> ================================ RESTART ================================
    >>> from stock import Stock
    >>> s = Stock('GOOG', 100, 490.1)
    >>> s.cost
    49010.0
    >>>
    Try calling s.cost() as a function and observe that it doesn't work now that cost has been defined as a property.
    
    >>> s.cost()
    ... fails ...
    >>>
    Making this change will likely break your earlier pcost.py program. You might need to go back and get rid of the () on the cost() method.
    
    Exercise 5.7: Properties and Setters
    Modify the shares attribute so that the value is stored in a private attribute and that a pair of property functions are used to ensure that it is always set to an integer value. Here is an example of the expected behavior:
    
    >>> ================================ RESTART ================================
    >>> from stock import Stock
    >>> s = Stock('GOOG',100,490.10)
    >>> s.shares = 50
    >>> s.shares = 'a lot'
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    TypeError: expected an integer
    >>>
    Exercise 5.8: Adding slots
    Modify the Stock class so that it has a __slots__ attribute. Then, verify that new attributes can't be added:
    
    >>> ================================ RESTART ================================
    >>> from stock import Stock
    >>> s = Stock('GOOG', 100, 490.10)
    >>> s.name
    'GOOG'
    >>> s.blah = 42
    ... see what happens ...
    >>>
    When you use __slots__, Python uses a more efficient internal representation of objects. What happens if you try to inspect the underlying dictionary of s above?
    
    >>> s.__dict__
    ... see what happens ...
    >>>
    It should be noted that __slots__ is most commonly used as an optimization on classes that serve as data structures. Using slots will make such programs use far-less memory and run a bit faster. You should probably avoid __slots__ on most other classes however.

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,6 @@
 # Exercise 3.3
 import csv
 
-
 def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=",", silence_errors=False):
     """
     Parse a CSV file into a list of records
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,52 +1,9 @@
-# pcost.py
-#
-# Exercise 1.27
-# with open("Data/portfolio.csv", 'rt') as file:
-#   total_cost = 0.0
-#   next(file)
-#   for line in file:
-#     parts = line.split(',')
-#     shares = int(parts[1])
-#     price = float(parts[2])
-#     total_cost += shares * price
-#   print(f'Total cost: {total_cost}')
-
-# Exercise 1.28
-# import gzip
-
-# with gzip.open("Data/portfolio.csv.gz", "rt") as file:
-#   for line in file:
-#     print(line, end='')
-
-
-# Exercise 1.30
-# def portfolio_cost(filename):
-#   total_cost = 0.0
-#   with open(filename, 'rt') as file:
-#     next(file)
-#     for line in file:
-#       parts = line.split(',')
-#       try:
-#         shares = int(parts[1])
-#         price = float(parts[2])
-#       except ValueError:
-#         print('Error: There was a problem with the data format in the file.')
-#         continue
-#       total_cost += shares * price
-#   return total_cost
-
-# cost = portfolio_cost('Data/missing.csv')
-# print('Total cost:', cost)
-
-# Exercise 1.32: Using a library function
-# Exercise 2.15: A practical enumerate() example
-
 import sys
 import report
 def portfolio_cost(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
     portfolio = report.read_portfolio(filename)
-    return sum(stock.cost() for stock in portfolio)
+    return sum(stock.cost for stock in portfolio)
 
 
 # if len(sys.argv) == 2:
